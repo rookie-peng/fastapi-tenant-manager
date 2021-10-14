e5216f97dde0a74a1f137b47618549c44678cb60
zhipeng.su <zhipeng.su@rootcloud.com>
2021-10-14 18:47:38 +0800

    修改callback 请求体格式

--- src/app/api/callback.py
+++ src/app/api/callback.py
@@ -6,7 +6,10 @@ import requests
 def callback(tenant, callback_body):
     time.sleep(3)
     # print("sleep 3s")
-    response = requests.post(tenant['callback'], data=callback_body, timeout=10)
+    response = requests.post(tenant['callback'], json=callback_body, timeout=10)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'}
+    # response = requests.post("http://127.0.0.1:9001", json=callback_body, headers=headers, timeout=10)
     logging.info(
         'wait for 3s, callback  iam-service-management for CMS resource application, resp status: %s',
         response.status_code)
